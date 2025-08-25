9c9809852a101a170fc76c6d11efe7956f4042aa
Luca <Luca@fanruan.com>
2025-08-25 16:42:06 +0800

    feat: 命令改成 jdk21的 & 测试成功输出成 json

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVA_OPTIONS="-server -XX:-RestrictContended -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms1096m -Xmx1096m"
+JAVA_OPTIONS="-server -XX:-RestrictContended -Xms1096m -Xmx1096m"
 
 if [[ "clean" == "$1" ]]; then
    mvn clean package
@@ -8,7 +8,8 @@ if [[ "clean" == "$1" ]]; then
 fi
 
 if [[ "gcprof" == "$1" ]]; then
-   JAVA_OPTIONS="$JAVA_OPTIONS -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+   # JDK9+ 统一用 -Xlog:gc 语法
+   JAVA_OPTIONS="$JAVA_OPTIONS -Xlog:gc*,safepoint:file=gc.log:time,uptime,level,tags"
    shift
 fi
 
@@ -19,16 +20,17 @@ if [[ "$2" == "-t" ]]; then
 fi
 
 if [[ "quick" == "$1" ]]; then
-   java -jar ./target/microbenchmarks.jar -jvmArgs "$JAVA_OPTIONS" -wi 3 -i 8 $JMH_THREADS -f 2 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -rf json -rff benchmark-results.json -jvmArgs "$JAVA_OPTIONS" -wi 3 -i 8 $JMH_THREADS -f 2 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "medium" == "$1" ]]; then
-   java -jar ./target/microbenchmarks.jar -jvmArgs "$JAVA_OPTIONS" -wi 3 -f 8 -i 6 $JMH_THREADS $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -rf json -rff benchmark-results.json -jvmArgs "$JAVA_OPTIONS" -wi 3 -f 8 -i 6 $JMH_THREADS $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "long" == "$1" ]]; then
-   java -jar ./target/microbenchmarks.jar -jvmArgs "$JAVA_OPTIONS" -wi 3 -i 15 $JMH_THREADS $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -rf json -rff benchmark-results.json -jvmArgs "$JAVA_OPTIONS" -wi 3 -i 15 $JMH_THREADS $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "profile" == "$1" ]]; then
-   java -server $JAVA_OPTIONS -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -r 5 -wi 3 -i 8 $JMH_THREADS -f 0 $2 $3 $4 $5 $6 $7 $8 $9
+   java -server $JAVA_OPTIONS -agentpath:/Applications/jprofiler/bin/macos/libjprofilerti.jnilib=port=8849 \
+        -jar ./target/microbenchmarks.jar -rf json -rff benchmark-results.json -r 5 -wi 3 -i 8 $JMH_THREADS -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "debug" == "$1" ]]; then
-   java -server $JAVA_OPTIONS -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y -jar ./target/microbenchmarks.jar -r 5 -wi 3 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
+   java -server $JAVA_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:8787 \
+        -jar ./target/microbenchmarks.jar -rf json -rff benchmark-results.json -r 5 -wi 3 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 else
-   java -jar ./target/microbenchmarks.jar -jvmArgs "$JAVA_OPTIONS" -wi 3 -i 15 -t 8 $1 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -rf json -rff benchmark-results.json -jvmArgs "$JAVA_OPTIONS" -wi 3 -i 15 -t 8 $1 $2 $3 $4 $5 $6 $7 $8 $9
 fi
-
